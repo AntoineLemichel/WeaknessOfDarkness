a6ca3b7fcdd64768f48b51b88d27141068205e8f
Tictac <paul.giraudet@gmail.com>
2018-11-15 15:53:48 +0100

    adding physics shot for starship

--- main.lua
+++ main.lua
@@ -5,6 +5,7 @@
  starship.y = 300
  starship.speed = 300
  starship.bullets = {}
+ starship.attacks = {}
 
 function love.load()
   love.window.setTitle("Weakness of Darkness")
@@ -13,10 +14,12 @@ function love.load()
  
   starship.image = love.graphics.newImage('/assets/pictures/ship/starship.png')
 
-  shoot = love.graphics.newVideo('/assets/pictures/ship/shotmagic.ogv')
+  magicshoot = love.graphics.newVideo('/assets/pictures/ship/shotmagic.ogv')
+  attackshoot = love.graphics.newImage('/assets/pictures/ship/shotatt3.png')
   enemyImage = love.graphics.newImage('/assets/pictures/ship/enemyattack.png')
   cooldown = 0
   cooldown2 = 0
+  cooldown3 = 0
   
 end
 
@@ -32,6 +35,15 @@ function love.update(dt)
     end
   end
   
+  for i,v in ipairs(starship.attacks) do
+    v.x = v.x + 1000 * dt
+    
+    if v.x >=900 then
+      table.remove(starship.attacks, i)
+    end
+  end
+  
+  
   if love.keyboard.isDown("up") then
     starship.y = starship.y - starship.speed * dt
   end
@@ -44,19 +56,29 @@ function love.update(dt)
   if love.keyboard.isDown("space") and cooldown == 0 then
     cooldown = 0.3
     bullet = {}
-    bullet.x = starship.x + 76
-    bullet.y = starship.y + 32.5
-    bullet.shoot = love.graphics.newVideo('/assets/pictures/ship/shotmagic.ogv')
+    bullet.x = starship.x + 54
+    bullet.y = starship.y + 28
+    bullet.magicshoot = love.graphics.newVideo('/assets/pictures/ship/shotmagic.ogv')
     table.insert(starship.bullets, bullet)
-    enemy = {}
-    enemy.x = 500
-    enemy.y = 300
-    enemy.image = love.graphics.newImage('/assets/pictures/ship/enemyattack.png')
-    table.insert(enemies, enemy)
   end
   
   cooldown2 = math.max(cooldown2 - dt,0)
-  if cooldown2 == 0 then
+  if love.keyboard.isDown('a') and cooldown2 == 0 then
+    cooldown2 = 0.15
+    attack1 = {}
+    attack1.x = starship.x + 50
+    attack1.y = starship.y + 10
+    attack1.attackshoot = love.graphics.newImage('/assets/pictures/ship/shotatt3.png')
+    table.insert(starship.attacks, attack1)
+    attack2 = {}
+    attack2.x = starship.x + 50
+    attack2.y = starship.y + 50
+    attack2.attackshoot = love.graphics.newImage('/assets/pictures/ship/shotatt3.png')
+    table.insert(starship.attacks, attack2)
+  end
+  
+  cooldown3 = math.max(cooldown3 - dt,0)
+  if cooldown3 == 0 then
     enemySpawn()
   end
   
@@ -75,20 +97,23 @@ function love.draw()
   end
   
   love.graphics.draw(starship.image, starship.x, starship.y, 0, 0.2, 0.2)
-    	
-  for i,v in ipairs(starship.bullets) do
-    love.graphics.draw(v.shoot, v.x, v.y, 0, 0.03, 0.03)
-    v.shoot:play()
-  end
-  for i,v in ipairs(starship.bullets) do
-    if shoot:isPlaying() then return end
-    shoot:rewind()
-    shoot:play()
-  end
   
   for i,v in ipairs(enemies) do
     love.graphics.draw(v.image, v.x, v.y, 0, 0.6, 0.6)
   end
+  for i,v in ipairs(starship.attacks) do
+    love.graphics.draw(v.attackshoot, v.x, v.y, 0, 0.3, 0.3)
+  end
+  for i,v in ipairs(starship.bullets) do
+    love.graphics.draw(v.magicshoot, v.x, v.y, 0, 0.03, 0.03)
+    v.magicshoot:play()
+  end
+  for i,v in ipairs(starship.bullets) do
+    if magicshoot:isPlaying() then return end
+    magicshoot:rewind()
+    magicshoot:play()
+  end
+
 end
 
 function backgroundVideo()
@@ -98,7 +123,7 @@ function backgroundVideo()
 end
 
 function enemySpawn ()
-  cooldown2 = 1
+  cooldown3 = 1
   enemy = {}
   enemy.x = 900
   enemy.y = math.random(550, 0)
