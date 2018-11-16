f00c91eb1f47e073c34039090a53822edd3ac15a
AntoineLemichek <antoine.lemichel@newgates.fr>
2018-11-16 09:22:03 +0100

    Adding conf for game

---
+++ conf.lua
@@ -0,0 +1,8 @@
+function love.conf(t)
+  t.window.title = "Weakness Of Darkness"
+  t.console = true
+  t.window.borderless = false
+  -- t.window.height = 500
+  -- t.window.width = 500
+  t.externalstorage = false
+end
