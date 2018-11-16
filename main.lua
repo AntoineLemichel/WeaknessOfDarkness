4630086d6fe69395ce3e76c53e200af7415455d3
Tictac <paul.giraudet@gmail.com>
2018-11-16 10:38:51 +0100

    adding random ball spawn on enemy kill

--- main.lua
+++ main.lua
@@ -1,4 +1,6 @@
 enemies = {}
+enemies.shot = {}
+orbs = {}
  
  starship = {}
  starship.x = 0
@@ -21,6 +23,7 @@ function love.load()
   magicshoot = love.graphics.newVideo('/assets/pictures/ship/shotmagic.ogv')
   attackshoot = love.graphics.newImage('/assets/pictures/ship/shotatt3.png')
   enemyImage = love.graphics.newImage('/assets/pictures/ship/enemyattack.png')
+  enemyImage2 = love.graphics.newImage('/assets/pictures/ship/enemymagic.png')
   cooldown = 0
   cooldown2 = 0
   cooldown3 = 0
@@ -32,19 +35,21 @@ function love.update(dt)
   backgroundVideo()
   bulletCollision()
 
-  for i,v in ipairs(starship.magics) do
-    v.x = v.x + 700 * dt
-    
-    if v.x >= 775 then
-      table.remove(starship.magics, i)
-    end
-  end
-
   for i,v in ipairs(enemies) do 
     v.x = v.x - 100 * dt
 
     if v.x <= - 2 then
       table.remove(enemies, i)
+    end
+  end
+  
+
+  
+  for i,v in ipairs(starship.magics) do
+    v.x = v.x + 700 * dt
+    
+    if v.x >= 775 then
+      table.remove(starship.magics, i)
     end
   end
   
@@ -56,11 +61,6 @@ function love.update(dt)
     end
   end
   
-  for i,v in ipairs(enemies) do
-    if v.x <= -2 then
-      table.remove(enemies, i)
-    end
-  end
   
   if love.keyboard.isDown("up") then
     starship.y = starship.y - starship.speed * dt
@@ -126,8 +126,17 @@ function love.draw()
   love.graphics.draw(starship.image, starship.x, starship.y, 0, 0.2, 0.2)
 
   for i,v  in ipairs(enemies) do 
-    love.graphics.draw(enemyImage, v.x, v.y, 0, 0.6, 0.6)
+    love.graphics.draw(v.image, v.x, v.y, 0, 0.6, 0.6)
   end
+  
+  for i,v  in ipairs(enemies.shot) do 
+    love.graphics.draw(v.enemyShot.image, v.x, v.y, 0, 0.6, 0.6)
+  end
+  
+  for i,v in ipairs(orbs) do
+    love.graphics.draw(v.image, v.x, v.y, 0, 0.2, 0.2)
+  end
+  
   
   for i,v in ipairs(starship.attacks) do
     love.graphics.draw(v.attackshoot, v.x, v.y, 0, 0.3, 0.3)
@@ -162,28 +171,45 @@ function enemySpawn ()
   enemy = {}
   enemy.x = 900
   enemy.y = math.random(550, 0)
-  enemy.image = love.graphics.newImage('/assets/pictures/ship/enemyattack.png')
+  enemy.type = love.math.random(0, 1)
+  enemy.enemyShot = {}
+  enemy.enemyShot.x = enemy.x
+  enemy.enemyShot.y = enemy.y
+  if enemy.type == 0 then
+    enemy.image = love.graphics.newImage('/assets/pictures/ship/enemyattack.png')
+    enemy.enemyShot.image = love.graphics.newImage('/assets/pictures/ship/enemyshotattack.png')
+  elseif enemy.type == 1 then
+    enemy.image = love.graphics.newImage('/assets/pictures/ship/enemymagic.png')
+    enemy.enemyShot.image = love.graphics.newImage('/assets/pictures/ship/enemyshotmagic.png')
+  end
+  --table.insert(enemies.shot, enemyShot)
   table.insert(enemies, enemy)
 end
 
 function bulletCollision()
 	for i,v in ipairs(enemies) do
+    
+          
 		for ia, va in ipairs(starship.magics) do
 			if va.x + 4 > v.x and
         va.x < v.x + 30 and
         va.y + 4 > v.y and
         va.y < v.y + 30 then
           
+          spawnOrbs(enemies[i].x, enemies[i].y)
+          
           score = score + 50
           table.remove(enemies, i)
           table.remove(starship.magics, ia)
-			end
+      end
 		end
     for ia, va in ipairs(starship.attacks) do
 			if va.x + 4 > v.x and
         va.x < v.x + 30 and
         va.y + 4 > v.y and
         va.y < v.y + 30 then
+          
+          spawnOrbs(enemies[i].x, enemies[i].y)
           
           score = score + 50
           table.remove(enemies, i)
@@ -192,3 +218,28 @@ function bulletCollision()
 		end
 	end
 end
+
+function spawnOrbs(x,y)
+      
+    randomOrbs = {}
+    randomOrbs.type = love.math.random(0, 100)
+    randomOrbs.x = x
+    randomOrbs.y = y
+    
+          if randomOrbs.type >2 and randomOrbs.type <10 then
+            randomOrbs.image = love.graphics.newImage('/assets/pictures/balls/blueBall.png')
+            table.insert(orbs, randomOrbs)
+          elseif randomOrbs.type >11 and randomOrbs.type <20 then
+            randomOrbs.image = love.graphics.newImage('/assets/pictures/balls/greenBall.png')
+            table.insert(orbs, randomOrbs)
+          elseif randomOrbs.type >21 and randomOrbs.type <30 then
+            randomOrbs.image = love.graphics.newImage('/assets/pictures/balls/yellowBall.png')
+            table.insert(orbs, randomOrbs)
+          elseif randomOrbs.type >31 and randomOrbs.type <40 then
+            randomOrbs.image = love.graphics.newImage('/assets/pictures/balls/redBall.png')
+            table.insert(orbs, randomOrbs)
+          end
+          
+          
+end
+        
